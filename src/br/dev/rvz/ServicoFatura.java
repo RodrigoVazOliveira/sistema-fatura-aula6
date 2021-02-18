50376e8bd22bc39a7716a4e9ad4917e5fca85561
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-18 16:07:33 -0300

    Refatorado para retornar todas faturas do consumidor

--- src/br/dev/rvz/ServicoFatura.java
+++ src/br/dev/rvz/ServicoFatura.java
@@ -6,13 +6,17 @@ import java.util.List;
 public class ServicoFatura {
     private static List<Fatura> faturas = new ArrayList<>();
 
-    public static Fatura pesquisarFaturaPeloEmailDoConsumidor(String email) throws Exception {
+    public static List<Fatura> pesquisarFaturaPeloEmailDoConsumidor(String email) throws Exception {
+        List<Fatura> faturas = new ArrayList<>();
         for(Fatura fatura : faturas) {
             if (fatura.getConsumidor().getEmail().equalsIgnoreCase(email)) {
-                return  fatura;
+                faturas.add(fatura);
             }
         }
-        throw new Exception("Fatura não existe!");
+        if (!faturas.isEmpty()) {
+            return faturas;
+        }
+        throw new Exception("Não existe fatura para esse consumidor!");
     }
 
     public static Fatura cadastrarFatura(String email, double valor, String dataVemcimento) throws Exception {
