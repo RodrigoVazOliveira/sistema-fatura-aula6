41bcc1ea505d67141b1a5a4f1b697c2b1a57a64e
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-18 14:00:02 -0300

    Criar class de serviço para consumidor

---
+++ src/br/dev/rvz/ServicoConsumidor.java
@@ -0,0 +1,8 @@
+package br.dev.rvz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicoConsumidor {
+    private static List<Consumidor> consumidores = new ArrayList<>();
+}
