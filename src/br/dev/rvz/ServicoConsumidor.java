37dad669d4b91f8e74d2472bf475b73a5ec701f8
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-18 14:36:54 -0300

    Criar método pesquisar consumidor por email no serviço de consumidor

--- src/br/dev/rvz/ServicoConsumidor.java
+++ src/br/dev/rvz/ServicoConsumidor.java
@@ -23,4 +23,14 @@ public class ServicoConsumidor {
             }
         }
     }
+
+    public static Consumidor pesquisarConsumidorPorEmail(String email) throws Exception{
+        validarEmail(email);
+        for (Consumidor consumidor : consumidores ) {
+            if (consumidor.getEmail().equalsIgnoreCase(email)) {
+                return consumidor;
+            }
+        }
+        throw new Exception("Nenhum consumidor foi localizado com e-mail " + email);
+    }
 }
