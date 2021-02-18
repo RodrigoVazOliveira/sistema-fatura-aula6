f7750bea05d4c7d557866e9739b7f4e47a386717
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-18 13:53:41 -0300

    Criar metodo de menu para o sistema

---
+++ src/br/dev/rvz/Sistema.java
@@ -0,0 +1,13 @@
+package br.dev.rvz;
+
+import java.util.Scanner;
+
+public class Sistema {
+    public static void menu() {
+        String mensagem = "1. Pesquisar por e-mail o consumidor\n";
+        System.out.println(mensagem);
+    }
+
+  
+
+}
