1e35d7c1a72abd626992b1a66ebcde359c723643
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-18 14:13:18 -0300

    Criar validação de e-mail do consumidor

--- src/br/dev/rvz/ServicoConsumidor.java
+++ src/br/dev/rvz/ServicoConsumidor.java
@@ -5,4 +5,15 @@ import java.util.List;
 
 public class ServicoConsumidor {
     private static List<Consumidor> consumidores = new ArrayList<>();
+
+    public static void validarEmail(String email) throws Exception {
+        if (!email.contains("@")){
+            throw new Exception("e-mail inválido!");
+        }
+        for(Consumidor consumidor : consumidores) {
+            if (consumidor.getEmail().equals(email)) {
+                throw new Exception("O E-mail " + email + " já existe");
+            }
+        }
+    }
 }
