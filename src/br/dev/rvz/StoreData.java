bb9e0ab8456bbcd45e39351333bee5535f09893b
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-19 07:24:50 -0300

    Enum para gravação dos arquivos

---
+++ src/br/dev/rvz/StoreData.java
@@ -0,0 +1,11 @@
+package br.dev.rvz;
+
+public enum StoreData {
+    CONSUMIDOR("consumidor.txt"), FATURA("fatura.txt");
+    private String nameFile;
+
+    StoreData(String nameFile) {
+        this.nameFile = nameFile;
+    }
+    
+}
