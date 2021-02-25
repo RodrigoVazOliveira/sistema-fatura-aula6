adebd793695c9414f521d65f23519485e72a6f76
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-25 10:47:51 -0300

    Refatorado par ao usuo da classe Controle para retirar os ifs

--- src/br/dev/rvz/Sistema.java
+++ src/br/dev/rvz/Sistema.java
@@ -12,48 +12,12 @@ public class Sistema {
         System.out.println(mensagem);
     }
 
-
-    private static List<Fatura> pesquisarFatura() throws Exception {
-        System.out.println("Digite o email: ");
-        String email = new Scanner(System.in).nextLine();
-        return ServicoFatura.pesquisarFaturaPeloEmailDoConsumidor(email);
-    }
-
-    private static Consumidor cadastrarConsumidor() throws Exception {
-        System.out.println("Por favor, digite o nome completo e o email: ");
-        Consumidor consumidor = ServicoConsumidor.cadastrarConsumidor(new Scanner(System.in).nextLine(),
-                new Scanner(System.in).nextLine());
-        return  consumidor;
-    }
-
-    private static Fatura cadastrarFatura() throws Exception {
-        System.out.println("Por favor, digite o email, valor e data de vencimento:");
-        Fatura fatura = ServicoFatura.cadastrarFatura(new Scanner(System.in).nextLine(),
-                new Scanner(System.in).nextDouble(),
-                new Scanner(System.in).nextLine());
-        return fatura;
-    }
-
     public static boolean executar() throws Exception {
         boolean continuar = true;
+        Controle controle = new Controle();
         while (continuar) {
             menu();
-            String resposta = new Scanner(System.in).nextLine();
-
-            if (resposta.equals("1")) {
-                List<Fatura> fatura = pesquisarFatura();
-                System.out.println(fatura);
-            } else if (resposta.equals("2")) {
-                Consumidor consumidor = cadastrarConsumidor();
-                System.out.println(consumidor);
-            } else if (resposta.equals("3")) {
-                Fatura fatura = cadastrarFatura();
-                System.out.println(fatura);
-            } else if (resposta.equals("4")) {
-                ServicoConsumidor.gravarListaConsumidorNoArquivo();
-                ServicoFatura.gravarListaDeFaturaNoArquivo();
-                continuar = false;
-            }
+            controle.selecionarOpcao(new Scanner(System.in).nextLine());
         }
         return continuar;
     }
